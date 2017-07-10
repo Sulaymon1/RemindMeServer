575d804e5760804a2a2a3ea37a0c21275577d9cd
Sulaymon <demirel6777@gmail.com>
2017-07-10 18:02:55 +0300

    Created base and controller

---
+++ src/main/java/com/sulaymon/remindme/server/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.sulaymon.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.sulaymon.remindme.server")
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+
+}
